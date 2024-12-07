c8a9a9311c86180496b6c998ae985df3cbefa2cc
Xinhe Wang <wangxinhe06@gmail.com>
2024-12-07 22:47:33 +0000

    Prune Day 7 Part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,15 +1,4 @@
 fun main() {
-    fun generateOperatorSequences(
-        n: Int, operations: List<String>
-    ): List<List<String>> {
-        if (n == 0) return listOf(emptyList())
-        return operations.flatMap { op ->
-            generateOperatorSequences(n - 1, operations).map {
-                it.toMutableList().apply { plusAssign(op) }
-            }
-        }
-    }
-
     fun solve(input: List<String>, check: (Long, List<Long>) -> Boolean): Long =
         input.sumOf { line ->
             line.split(": ").let { (v, nums) ->
@@ -17,30 +6,19 @@ fun main() {
             }.let { (v, nums) -> if (check(v, nums)) v else 0 }
         }
 
-    fun part1(input: List<String>): Long =
-        solve(input) { v, nums ->
-            (0..<(1 shl (nums.size - 1))).any { s ->
-                nums.reduceIndexed { i, acc, n ->
-                    if (s and (1 shl (i - 1)) == 0) acc + n
-                    else acc * n
-                } == v
-            }
+    fun part1(v: Long, nums: List<Long>): Boolean =
+        (0..<(1 shl (nums.size - 1))).any { s ->
+            nums.reduceIndexed { i, acc, n ->
+                if (s and (1 shl (i - 1)) == 0) acc + n
+                else acc * n
+            } == v
         }
 
-    fun part2(input: List<String>): Long =
-        solve(input) { v, nums ->
-            generateOperatorSequences(
-                nums.size - 1, listOf("+", "*", "||")
-            ).any { s ->
-                nums.reduceIndexed { i, acc, n ->
-                    when (s[i - 1]) {
-                        "+" -> acc + n
-                        "*" -> acc * n
-                        "||" -> (acc.toString() + n).toLong()
-                        else -> error("Unknown operation")
-                    }
-                } == v
-            }
+    fun part2(v: Long, nums: List<Long>): Boolean =
+        if (nums.size == 1) nums[0] == v
+        else nums.let { (a, b) ->
+            listOf(a + b, a * b, (a.toString() + b).toLong())
+                .any { part2(v, listOf(it) + nums.drop(2)) }
         }
 
     val testInput = listOf(
@@ -54,10 +32,10 @@ fun main() {
         "21037: 9 7 18 13",
         "292: 11 6 16 20",
     )
-    check(part1(testInput) == 3749L)
-    check(part2(testInput) == 11387L)
+    check(solve(testInput, ::part1) == 3749L)
+    check(solve(testInput, ::part2) == 11387L)
 
     val input = readInput("Day07")
-    part1(input).println()
-    part2(input).println()
+    solve(input, ::part1).println()
+    solve(input, ::part2).println()
 }
